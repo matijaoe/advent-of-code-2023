4bb3b25620b4065277a951b88179f246a0eaf8df
Matija Osrečki <github@matijao.com>
2023-12-04 17:52:47 +0100

    wip day 4 part 2

--- src/day-04/index.ts
+++ src/day-04/index.ts
@@ -1,44 +1,14 @@
 import { parseLines, readInput } from 'io'
-import { sum } from 'utils'
+import { sum, unique } from 'utils'
 
 const input = await readInput('day-04', 'example')
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// ------------------------- Part 1 ---------------------------
+// ------------------------- Shared ---------------------------
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-export const part1 = () => {
-  const lines = parseLines(input)
-
-  const total = lines.reduce((acc, line) => {
-    const [_, lottery] = line.split(': ')
-    const [winningNumbers, chosenNumbers] = lottery
-      .trim()
-      .split(' | ')
-      .map((str) => str.split(/\s+/).map(Number))
-
-    const myMatches = chosenNumbers.filter((num) => winningNumbers.includes(num))
-    const matchCount = myMatches.length
-
-    if (matchCount > 0) {
-      const points = 2 ** (matchCount - 1)
-      return acc + points
-    }
-
-    return acc
-  }, 0)
-
-  return total
-}
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// ------------------------- Part 2 ---------------------------
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-export const part2 = () => {
-  const lines = parseLines(input)
-
-  const totalScratchcards = lines.reduce((acc, line) => {
+const countCardMatches = (lines: string[]) => {
+  return lines.reduce((acc, line) => {
     const [cardMeta, lottery] = line.split(': ')
     const [_, cardNumberStr] = cardMeta.split(/\s+/)
     const cardNumber = Number(cardNumberStr)
@@ -50,39 +20,88 @@ export const part2 = () => {
     const myMatches = chosenNumbers.filter((num) => winningNumbers.includes(num))
     const matchCount = myMatches.length ?? 0
 
+    acc[cardNumber] = matchCount
+
+    return acc
+  }, {} as Record<string, number>)
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ------------------------- Part 1 ---------------------------
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+export const part1 = () => {
+  const lines = parseLines(input)
+
+  const cardMatches = countCardMatches(lines)
+
+  return Object.values(cardMatches).reduce((acc, count) => {
+    const points = 2 ** (count - 1)
+    return acc + points
+  }, 0)
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ------------------------- Part 2 ---------------------------
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+export const part2 = () => {
+  const lines = parseLines(input).slice(0, 30)
+
+  const cardMatches = countCardMatches(lines)
+  /* {
+    "1": 4,
+    "2": 2,
+    "3": 2,
+    "4": 1,
+    "5": 0,
+    "6": 0
+  } */
+
+  const matchCountEntries = (Object.entries(cardMatches) as Array<[string, number]>).map(([cardNumberStr, count]) => [Number(cardNumberStr), count])
+
+  const calcStuff = (acc: Record<string, number>, cardNumber: number, count: number) => {
     acc[cardNumber] ??= 0
-
-    console.log('\ncard :', cardNumber, '| matched:', matchCount, '| total:', acc[cardNumber])
-
-    console.log('adding 1 to :', cardNumber)
     acc[cardNumber] += 1
 
-    // create an arary of lenght of matchCount, and add 1 + gameNumber to each index
-    const wonCards: number[] = Array.from({ length: matchCount }, (_, i) => i + 1 + cardNumber)
+    if (acc[cardNumber] === 0 || !(cardNumber in acc)) { return acc }
 
-    wonCards.forEach((card) => {
-      acc[card] ??= 0
-      console.log('adding 1 to :', card)
-      acc[card] += 1
+    const arr = unique(Array.from({ length: count }, (_, i) => i + 1 + cardNumber).filter((num) => num < lines.length))
 
-      const cardCountCopy = Math.max(0, acc[card] - 1)
-      while (cardCountCopy > 0) {
-        // todo: extract all this into separate reusable function
-        // or, first store all counts of every card, and then loop over them
-      }
+    arr.forEach((num) => {
+      acc[num] ??= 0
+      acc[num] += 1
+
+      return calcStuff(acc, num, count - 1)
     })
-    console.log(acc)
-    console.log('-------------------')
+
+    return acc
+  }
+
+  const reduced = matchCountEntries.reduce((acc, [cardNumber, count]) => {
+    calcStuff(acc, cardNumber, count - 1)
+    // const arr = unique(Array.from({ length: count }, (_, i) => i + 1 + cardNumber).filter((num) => num < lines.length))
+
+    // acc[cardNumber] ??= 0
+    // acc[cardNumber] += 1
+
+    // arr.forEach((card) => {
+    //   acc[card] ??= 0
+    //   acc[card] += 1
+
+    //   calcStuff(acc, card, count - 1)
+    //   // console.log(acc)
+    // })
 
     return acc
   }, {} as Record<string, number>)
 
-  console.log(totalScratchcards)
+  // console.log(reduced)
 
-  return sum(Object.values(totalScratchcards))
+  return sum(Object.values(reduced))
 }
 
-console.log(part2()) // 102592 too low
+// console.log(part2()) // 102592 too low
 
 /*
 expected:
