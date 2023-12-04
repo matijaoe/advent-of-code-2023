f708f9767e29d074e80ce85c58b8126dd323826c
Matija Osrečki <github@matijao.com>
2023-12-04 12:27:39 +0100

    Work on day 4 part 2

--- src/day-04/index.ts
+++ src/day-04/index.ts
@@ -1,7 +1,11 @@
 import { parseLines, readInput } from 'io'
-import { unique } from 'utils'
+import { sum } from 'utils'
 
-const input = await readInput('day-04', 'input')
+const input = await readInput('day-04', 'example')
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ------------------------- Part 1 ---------------------------
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 export const part1 = () => {
   const lines = parseLines(input)
@@ -26,3 +30,68 @@ export const part1 = () => {
 
   return total
 }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ------------------------- Part 2 ---------------------------
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+export const part2 = () => {
+  const lines = parseLines(input)
+
+  const totalScratchcards = lines.reduce((acc, line) => {
+    const [cardMeta, lottery] = line.split(': ')
+    const [_, cardNumberStr] = cardMeta.split(/\s+/)
+    const cardNumber = Number(cardNumberStr)
+    const [winningNumbers, chosenNumbers] = lottery
+      .trim()
+      .split(' | ')
+      .map((str) => str.split(/\s+/).map(Number))
+
+    const myMatches = chosenNumbers.filter((num) => winningNumbers.includes(num))
+    const matchCount = myMatches.length ?? 0
+
+    acc[cardNumber] ??= 0
+
+    console.log('\ncard :', cardNumber, '| matched:', matchCount, '| total:', acc[cardNumber])
+
+    console.log('adding 1 to :', cardNumber)
+    acc[cardNumber] += 1
+
+    // create an arary of lenght of matchCount, and add 1 + gameNumber to each index
+    const wonCards: number[] = Array.from({ length: matchCount }, (_, i) => i + 1 + cardNumber)
+
+    wonCards.forEach((card) => {
+      acc[card] ??= 0
+      console.log('adding 1 to :', card)
+      acc[card] += 1
+
+      const cardCountCopy = Math.max(0, acc[card] - 1)
+      while (cardCountCopy > 0) {
+        // todo: extract all this into separate reusable function
+        // or, first store all counts of every card, and then loop over them
+      }
+    })
+    console.log(acc)
+    console.log('-------------------')
+
+    return acc
+  }, {} as Record<string, number>)
+
+  console.log(totalScratchcards)
+
+  return sum(Object.values(totalScratchcards))
+}
+
+console.log(part2()) // 102592 too low
+
+/*
+expected:
+{
+  "1": 1,
+  "2": 2,
+  "3": 4,
+  "4": 8,
+  "5": 14,
+  "6": 1
+}
+*/
